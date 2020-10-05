1c620526b0f1f065dccb2cd8c68689f71003329e
KillerDarkWolf <tristancc99@gmail.com>
2020-10-05 12:27:26 -0400

    init

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ app.get("/", (req, res, next) => {
 
 const port = process.env.PORT || 3000;
 
-db.sync().then(() => {
+
   app.listen(port, () => {
     console.log(`listening on port ${port}`);
   });
-});
